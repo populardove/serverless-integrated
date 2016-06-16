b15201a66de85d1b8fdef4d26aaf48f68aaaaf90
Georgios Kiragiannis <geo@Georgioss-MacBook-Pro.local>
2016-06-15 21:10:26 -0400

    first commit

---
+++ restApi/lib/index.js
@@ -0,0 +1,36 @@
+/**
+ * Lib
+ */
+
+// Single - All
+module.exports.singleAll = function(event, cb) {
+
+  var response = {
+    message: 'Your Serverless function ran successfully via the \''
+    + event.httpMethod
+    + '\' method!'
+  };
+
+  return cb(null, response);
+};
+
+// Multi - Create
+module.exports.multiCreate = function(event, cb) {
+
+  var response = {
+    message: 'Your Serverless function \'multi/create\' ran successfully!'
+  };
+
+  return cb(null, response);
+};
+
+// Multi - Show
+module.exports.multiShow = function(event, cb) {
+
+  var response = {
+    message: 'Your Serverless function \'multi/show\' ran successfully with the following ID \'' + event.pathId + '\'!',
+    event: event
+  };
+
+  return cb(null, response);
+};
